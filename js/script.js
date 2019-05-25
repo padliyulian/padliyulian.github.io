39d8e69af6744584aafa98163d9bd19aa0254e32
padli yulian <padliyulian@ymail.com>
2019-05-25 21:59:00 +0700

    v1.3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 window.onscroll = function() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document
-      .querySelector("#julian-navbar-onscroll")
+      .querySelector("#jn-onscroll")
       .classList.remove("elevation-0");
   } else {
     document
-      .querySelector("#julian-navbar-onscroll")
+      .querySelector("#jn-onscroll")
       .classList.add("elevation-0");
   }
 };
